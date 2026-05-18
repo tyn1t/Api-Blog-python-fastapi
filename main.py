a20540ef84706d2aa3a5640a00884d4ec98be8c5
giovanni souza <anderlansouza71@gmail.com>
2026-05-18 20:47:19 -0300

    FUNC TEST HELLOR

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ app = FastAPI()
 @app.get("/")
 async def hello_test(): 
     return {"MENSSAGEM":"oLA MUNDO"}
+
 @app.get("/info")
 def set_domin():
     return {"domain": settings.api_domain, "url": settings.base_url}
