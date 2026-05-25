696ad8ef9dd04d51b4fa640f48a8f6fe77e5af49
giovanni souza <anderlansouza71@gmail.com>
2026-05-25 00:54:30 -0300

    send

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
     
     upload_dir: str
   
